bef4c4c63cc943fd48a0289d22babbd3e2173193
AlexGavalas <alex.gavalas.92@gmail.com>
2020-04-05 21:12:16 +0300

    refactor (dont cache in sw html files)

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -31,6 +31,9 @@ const plugins = [
     }),
     new HtmlWebpackChangeAssetsExtensionPlugin(),
     new WorkboxPlugin.GenerateSW({
+        exclude: [
+            /\.html$/,
+        ],
         runtimeCaching: [
             {
                 urlPattern: /\.br$/,
